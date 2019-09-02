77c02eda17af01b2d836af9722b6f14c9d8fcef1
casio <carsten.kraus@gmail.com>
2019-09-02 16:03:08 +0200

    explicitly require png - no luck yet, tho: white box instead

--- js/ARScene.js
+++ js/ARScene.js
@@ -1,8 +1,8 @@
 /* global require module */
 
-import React, { Component } from 'react'
+import React, {Component} from 'react';
 
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 import {
   ViroNode,
@@ -10,51 +10,51 @@ import {
   Viro3DObject,
   ViroAmbientLight,
   ViroARTrackingTargets,
-  ViroARImageMarker
-} from 'react-viro'
+  ViroARImageMarker,
+} from 'react-viro';
 
 export default class HelloWorldSceneAR extends Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    console.log(props)
+    console.log(props);
   }
 
   render() {
-    const log = context => content => console.log(context, content)
+    const log = context => content => console.log(context, content);
 
     ViroARTrackingTargets.createTargets({
       target: {
         source: require('./res/patterina_anchor.png'),
         orientation: 'UP',
         physicalWidth: 0.138,
-        type: 'Image'
-      }
-    })
+        type: 'Image',
+      },
+    });
 
-    console.log(this.props.pauseUpdates)
+    console.log(this.props.pauseUpdates);
 
     return (
       <ViroARScene onTrackingUpdated={log('tracking')}>
         <ViroAmbientLight color="#ff0000" />
         <ViroARImageMarker
           target={'target'}
-          pauseUpdates={this.props.pauseUpdates}
-        >
+          pauseUpdates={this.props.pauseUpdates}>
           <ViroNode>
             <Viro3DObject
               source={require('./res/latzhose-png.obj')}
+              resources={[require('./res/latzhose-72dpi.png')]}
               type="OBJ"
             />
           </ViroNode>
         </ViroARImageMarker>
       </ViroARScene>
-    )
+    );
   }
 }
 
 HelloWorldSceneAR.propTypes = {
-  pauseUpdates: PropTypes.bool
-}
+  pauseUpdates: PropTypes.bool,
+};
 
-module.exports = HelloWorldSceneAR
+module.exports = HelloWorldSceneAR;
