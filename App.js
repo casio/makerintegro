eb173ccee3a081753250f15234e7a63cc7332ad3
casio <carsten.kraus@gmail.com>
2019-09-02 15:42:25 +0200

    transfer app code

--- App.js
+++ App.js
@@ -1,114 +1,58 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+/* global module */
 
-import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, {Component} from 'react';
+import ARScene from './js/ARScene.js';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {View, Button, StyleSheet} from 'react-native';
 
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
+import {ViroARSceneNavigator} from 'react-viro';
+
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      navigatorProps: {
+        apiKey: '1E250084-FB3D-4E19-9866-05258CA018F4',
+        autofocus: true,
+        numberOfTrackedImages: 1,
+      },
+      pauseUpdates: false,
+    };
+  }
+
+  toggleUpdates() {
+    this.setState({pauseUpdates: !this.state.pauseUpdates});
+  }
+
+  render() {
+    return (
+      <View style={styles.wrapper}>
+        <ViroARSceneNavigator
+          {...this.state.navigator}
+          initialScene={{
+            scene: () => <ARScene pauseUpdates={this.state.pauseUpdates} />,
+          }}
+        />
+        <Button
+          style={styles.text}
+          onPress={this.toggleUpdates.bind(this)}
+          title={`pauseUpdates is ${this.state.pauseUpdates}`}
+        />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  wrapper: {
+    flex: 1,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  text: {
+    padding: 10,
+    color: 'white',
+    backgroundColor: 'red',
   },
 });
 
-export default App;
+module.exports = App;
---
+++ js/ARScene.js
@@ -0,0 +1,60 @@
+/* global require module */
+
+import React, { Component } from 'react'
+
+import PropTypes from 'prop-types'
+
+import {
+  ViroNode,
+  ViroARScene,
+  Viro3DObject,
+  ViroAmbientLight,
+  ViroARTrackingTargets,
+  ViroARImageMarker
+} from 'react-viro'
+
+export default class HelloWorldSceneAR extends Component {
+  constructor(props) {
+    super(props)
+
+    console.log(props)
+  }
+
+  render() {
+    const log = context => content => console.log(context, content)
+
+    ViroARTrackingTargets.createTargets({
+      target: {
+        source: require('./res/patterina_anchor.png'),
+        orientation: 'UP',
+        physicalWidth: 0.138,
+        type: 'Image'
+      }
+    })
+
+    console.log(this.props.pauseUpdates)
+
+    return (
+      <ViroARScene onTrackingUpdated={log('tracking')}>
+        <ViroAmbientLight color="#ff0000" />
+        <ViroARImageMarker
+          target={'target'}
+          pauseUpdates={this.props.pauseUpdates}
+        >
+          <ViroNode>
+            <Viro3DObject
+              source={require('./res/latzhose-png.obj')}
+              type="OBJ"
+            />
+          </ViroNode>
+        </ViroARImageMarker>
+      </ViroARScene>
+    )
+  }
+}
+
+HelloWorldSceneAR.propTypes = {
+  pauseUpdates: PropTypes.bool
+}
+
+module.exports = HelloWorldSceneAR
